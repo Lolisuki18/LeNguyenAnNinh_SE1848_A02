f383983e96f524eef67678c97a0b72b455643f2b
Lolisuki18 <leninh2004@gmail.com>
2025-07-01 13:17:21 +0700

    ChangeConnectDatabase

--- BusinessObject/LucyContext.cs
+++ BusinessObject/LucyContext.cs
@@ -31,7 +31,7 @@ public partial class LucyContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost,1433;Database=Lucy_SalesData;Uid=sa;Pwd=12345;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Server=ADMIN-PC\\NINHLE;Database=Lucy_SalesData;User Id=sa;Password=12345;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
